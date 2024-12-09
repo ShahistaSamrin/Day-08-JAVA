38e37077ef7617bf495c48db4cfc6a2deab394d5
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-09 19:26:30 +0530

    Add files via upload

---
+++ twodimensionalarray.java
@@ -0,0 +1,12 @@
+/* [1, 2]
+   [3, 4]   */
+import java.util.*;
+public class twodimensionalarray{
+    public static void main(String args[]){
+        //initilization of 2D array
+        int a[][]={{1,2},{3,4}};
+        //accessing 1D array in 2D array
+        System.out.println(Arrays.toString(a[0]));
+        System.out.println(Arrays.toString(a[1]));
+    }
+}
